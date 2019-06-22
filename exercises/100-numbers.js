c7b8b1730987fc864e1d9f942e009bdde94bc3ae
Okera Bishop <obishop25@yahoo.com>
2019-06-22 14:18:15 -0500

    changes to class exercise

--- exercises/100-numbers.js
+++ exercises/100-numbers.js
@@ -3,20 +3,30 @@
 // Declare a variable "myNum" and assign any number to it, then return "myNum".
 // NOTE: "myNum" can be any valid JavaScript number
 function makeANumber () {
+    let myNum = 5;
+    return myNum;
 
 }
 
 // Declare a variable "myInt" and assign an integer to it, then return "myInt"
 function makeAnInteger () {
+    let myInt = 7;
+    return myInt;
+
 
 }
 
 // Declare a variable "myFloat" and assign a floating point number to it, then return "myFloat"
 function makeAFloat () {
+    let myFloat = 8.3;
+    return myFloat; 
+    
 
 }
 
 // Declare a variable "zilch" and assign the number 0 to it, then return "zilch"
 function makeZero () {
+    let zilch = 0;
+    return zilch; 
 
 }
--- exercises/104-strings.js
+++ exercises/104-strings.js
@@ -1,9 +1,16 @@
+//import { METHODS } from "http";
+
 // Let's venture into JavaScript strings in this exercise.
 // Hold onto your hats: we will be using functions with parameters here too.
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 // 1) Create a function "helloWorld"
 // 2) Return the string "Hello, world!"
+function helloWorld () {
+  return "Hello, world!"
+
+
+}
 
 
 // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
@@ -12,6 +19,7 @@
 // 3) Return the string "Hello, <name>!" where <name> is the value passed to the function
 
 function helloName (name) {
+  return "Hello, " + name + "!"
 
 }
 
@@ -25,6 +33,8 @@ function abstractLength () {
     'Following Brooks we distinguish accidental from essential difficulty, but disagree with his premise that most complexity remaining in contemporary systems is essential. ' +
     'We identify common causes of complexity and discuss general approaches which can be taken to eliminate them where they are accidental in nature. ' +
     'To make things more concrete we then give an outline for a potential complexity-minimizing approach based on functional programming and Codd’s relational model of data.'
+    return tarPitAbstract.length
+
 
 }
 
